f502714e4558be7f2439c1927660b2aff30f9571
Jerikka <jerikka.harris@gmail.com>
2023-08-23 20:25:24 -0500

    update src/index.js to include login in children

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { App } from "./components";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Products from "./components/Products";
 
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
@@ -16,17 +17,9 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [{ path: "/", element: <Home /> }, { path: "/login", element: <Login /> }, { path: "/register", element: <Register /> }],
   },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-]);
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
