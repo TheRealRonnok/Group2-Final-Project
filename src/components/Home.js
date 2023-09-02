6ddd6176756140f19e6618b624186bcefee79296
Ronnok <konnor.millerson@hotmail.com>
2023-09-02 18:09:43 -0500

    Finalized many, many things.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,17 @@ import React from "react";
 import { NavLink, useOutletContext } from "react-router-dom";
 
 const Home = () => {
-  
   return (
     <>
-      <p>Setting up basic stuff</p>
+      <h1>Welcome to Marvel Masters!</h1>
+      <br></br>
+      <br></br>
+      <p>
+        We're passionate about selling quality action figures at competitive
+        prices to ensure any fan has access to their favorite Marvel heroes!
+      </p>
+
+      <p>Browse our Products page to find your hero today!</p>
     </>
   );
 };
